1f9cf386f4d65c01984caa203b12ecdec812ebcf
mahdi <bitsgenix@gmail.com>
2025-03-28 17:28:59 +0330

    fix: fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://glandjs.github.io/docs',
-  base: '/docs/',
+  site: 'https://glandjs.github.io',
+  base: '/',
 })
