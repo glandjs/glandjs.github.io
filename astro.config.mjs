54ce72d46b45bcf047c18dbe58962c1fe0572844
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-04-04 13:14:39 +0330

    chore: delete all the files, I'll get them back!

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config'
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://glandjs.github.io/docs',
-  base: '/docs/',
-})
